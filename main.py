ce224fdd5c7d8e6002916e28f842f0b42b8f2943
Asher Mancinelli <ashermancinelli@gmail.com>
2019-09-19 11:14:24 -0700

    working firebase integration

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/add_set')
+def addit():
+    return render_template('add_set.html')
+
+@app.route('/view_all')
+def viewit():
+    return render_template('view_all.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
--- static/script.js
+++ static/script.js
@@ -1,3 +1,27 @@
-'use strict';
-
-window.addEventListener('load', () => { });
+'use strict'; 
+window.addEventListener('load', () => { 
+    var b = firebase.database().ref("set");
+    if ($('#addSet').length > 0) {
+        $("#addSet").submit((a) => {
+            $(this), console.log("Pushing to firebase");
+            var reps = $("#reps").val(),
+                weight = $("#weight").val(),
+                lift = $("#lift").val(),
+                date = $("#date").val(),
+                name = $("#name").val(),
+                set = {
+                    date: date,
+                    name: name,
+                    reps: reps,
+                    lift: lift,
+                    weight: weight
+                };
+            console.log("Name: ", name);
+            return b.push(set).then((a) => {
+                // display sucess message or something
+                // $(".sucess").css("display", "block");
+                // $(".sucess-none").css("display", "none");
+                alert('Successfully submitted data.');
+            }), false })
+    }
+});
