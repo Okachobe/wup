8b15f735805d8a0ab579a89ef254743428cdb734
Asher Mancinelli <ashermancinelli@gmail.com>
2019-09-24 15:51:30 -0700

    added page for requesting new movements"

--- main.py
+++ main.py
@@ -13,5 +13,9 @@ def addit():
 def viewit():
     return render_template('view_all.html')
 
+@app.route('/request_movement')
+def reqit():
+    return render_template('request_movement.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
